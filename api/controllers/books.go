7859f45ee74954564591787e8c4bb64991c039cb
Himanshu Gupta <60879011+err-him@users.noreply.github.com>
2020-03-03 13:54:15 +0530

    Feature book api (#6)
    * added book api handler
    
    * added transaction helper with book add Api
    
    * added book delete api
    
    * added book search and delete api
    
    * update readeMe for book api

---
+++ api/controllers/books.go
@@ -0,0 +1,166 @@
+package controllers
+
+import (
+	hc "book-store-api/api/constants"
+	"book-store-api/api/handler"
+	"book-store-api/api/models"
+	r "book-store-api/api/repositories"
+	"book-store-api/config/driver"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
+)
+
+type Books struct {
+	bookRepo models.BooksRepo
+}
+
+func NewBookHandler(db *driver.DB) *Books {
+	return &Books{
+		bookRepo: r.NewBookRepo(db.SQL),
+	}
+}
+
+/**
+ * [func description]
+ * @param  {[type]} b [description]
+ * @return {[type]}   [description]
+ */
+func (b *Books) AddBook(w http.ResponseWriter, r *http.Request) {
+	req := models.Books{}
+	err := json.NewDecoder(r.Body).Decode(&req)
+	fmt.Println("err", err)
+	if err != nil {
+		handler.HttpError(w, http.StatusBadRequest, hc.BAD_REQUEST, err)
+		return
+	}
+	//validate the request
+	if req.Name == nil || req.Image == nil || req.ISBN == nil || req.Prices.OldPrice == nil || req.Prices.NewPrice == nil || req.Language == nil || req.PublisherId == nil || req.PublishedAt == nil || req.BookGenre.Id == nil || req.BookAuthor.Id == nil || req.Other.Quantity == nil || req.Other.Type == nil || req.Other.NumberPages == nil {
+		handler.HttpError(w, http.StatusBadRequest, hc.BAD_REQUEST, err)
+		return
+	}
+	res, err := b.bookRepo.Add(r.Context(), &req)
+	if err != nil {
+		handler.HttpError(w, http.StatusInternalServerError, err.Error(), err.Error())
+		return
+	}
+	handler.HttpResponse(w, http.StatusCreated, res)
+}
+
+/**
+ * [func description]
+ * @param  {[type]} b [description]
+ * @return {[type]}   [description]
+ */
+func (b *Books) UpdateBook(w http.ResponseWriter, r *http.Request) {
+	req := models.Books{}
+	err := json.NewDecoder(r.Body).Decode(&req)
+	if err != nil {
+		handler.HttpError(w, http.StatusBadRequest, hc.BAD_REQUEST, err)
+		return
+	}
+	//validate the request
+	if req.Id == 0 || req.Image == nil || req.Status == nil || req.Name == nil || req.ISBN == nil || req.Prices.OldPrice == nil || req.Prices.NewPrice == nil || req.Language == nil || req.PublisherId == nil || req.PublishedAt == nil || req.BookGenre.Id == nil || req.BookAuthor.Id == nil || req.Other.Quantity == nil || req.Other.Type == nil || req.Other.NumberPages == nil {
+		handler.HttpError(w, http.StatusBadRequest, hc.BAD_REQUEST, err)
+		return
+	}
+	res, err := b.bookRepo.Update(r.Context(), &req)
+	if err != nil {
+		handler.HttpError(w, http.StatusInternalServerError, err.Error(), err.Error())
+		return
+	}
+	handler.HttpResponse(w, http.StatusNoContent, res)
+}
+
+/**
+ * [func description]
+ * @param  {[type]} g [description]
+ * @return {[type]}   [description]
+ */
+func (b *Books) DeleteBook(w http.ResponseWriter, r *http.Request) {
+
+	vars := mux.Vars(r)
+	id, err := strconv.ParseInt(vars["id"], 10, 64)
+	if err != nil {
+		handler.HttpError(w, http.StatusBadRequest, hc.BAD_REQUEST, err.Error())
+		return
+	}
+	res, err := b.bookRepo.Delete(r.Context(), id)
+	if err != nil {
+		handler.HttpError(w, http.StatusInternalServerError, err.Error(), err.Error())
+		return
+	}
+	handler.HttpResponse(w, http.StatusNoContent, res)
+}
+
+/**
+ * [func description]
+ * @param  {[type]} g [description]
+ * @return {[type]}   [description]
+ */
+func (b *Books) GetOne(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id, err := strconv.ParseInt(vars["id"], 10, 64)
+	if err != nil {
+		handler.HttpError(w, http.StatusBadRequest, hc.BAD_REQUEST, err.Error())
+		return
+	}
+	res, err := b.bookRepo.GetBookDetailById(r.Context(), id)
+	if err != nil {
+		handler.HttpError(w, http.StatusInternalServerError, err.Error(), err.Error())
+		return
+	}
+	handler.HttpResponse(w, http.StatusOK, res)
+}
+
+/**
+ * [func description]
+ * @param  {[type]} p [description]
+ * @return {[type]}   [description]
+ */
+func (b *Books) GetAll(w http.ResponseWriter, r *http.Request) {
+	//get the query params
+	var limit, offset int64
+	if r.URL.Query().Get("limit") == "" {
+		limit = hc.LIMIT
+	} else {
+		limit, _ = strconv.ParseInt(r.URL.Query().Get("limit"), 10, 64)
+	}
+
+	if r.URL.Query().Get("offset") == "" {
+		offset = hc.OFFSET
+	} else {
+		offset, _ = strconv.ParseInt(r.URL.Query().Get("offset"), 10, 64)
+	}
+	res, err := b.bookRepo.GetAll(r.Context(), limit, offset)
+	if err != nil {
+		handler.HttpError(w, http.StatusInternalServerError, err.Error(), err.Error())
+		return
+	}
+	handler.HttpResponse(w, http.StatusOK, res)
+}
+
+/**
+ * [func description]
+ * @param  {[type]} p [description]
+ * @return {[type]}   [description]
+ */
+func (b *Books) SearchBook(w http.ResponseWriter, r *http.Request) {
+	//get the query params
+	var query string
+	if r.URL.Query().Get("q") == "" {
+		handler.HttpError(w, http.StatusInternalServerError, hc.INVALID_SEARCH_PARAM, nil)
+		return
+	} else {
+		query = r.URL.Query().Get("q")
+	}
+	res, err := b.bookRepo.SearchBookByName(r.Context(), query)
+	if err != nil {
+		handler.HttpError(w, http.StatusInternalServerError, err.Error(), err.Error())
+		return
+	}
+	handler.HttpResponse(w, http.StatusOK, res)
+}
--- config/routes.go
+++ config/routes.go
@@ -13,6 +13,7 @@ func handleAppRoutes(r *mux.Router, db *driver.DB) {
 	genreHandler := controllers.NewGenreHandler(db)
 	pubHandler := controllers.NewPublisherHandler(db)
 	authorhandler := controllers.NewAuthorHandler(db)
+	bookHandler := controllers.NewBookHandler(db)
 	//handling API versioning
 	v1 := r.PathPrefix("/api/v1").Subrouter()
 
@@ -36,4 +37,12 @@ func handleAppRoutes(r *mux.Router, db *driver.DB) {
 	v1.HandleFunc("/author/get/{id}", authorhandler.GetOne).Methods(http.MethodGet)
 	v1.HandleFunc("/author/delete/{id}", authorhandler.Delete).Methods(http.MethodDelete)
 	v1.HandleFunc("/author/update", authorhandler.UpdateAuthor).Methods(http.MethodPut)
+
+	//Book Routes
+	v1.HandleFunc("/book/add", bookHandler.AddBook).Methods(http.MethodPost)
+	v1.HandleFunc("/book/update", bookHandler.UpdateBook).Methods(http.MethodPut)
+	v1.HandleFunc("/book/delete/{id}", bookHandler.DeleteBook).Methods(http.MethodDelete)
+	v1.HandleFunc("/book/get/all", bookHandler.GetAll).Methods(http.MethodGet)
+	v1.HandleFunc("/book/get/{id}", bookHandler.GetOne).Methods(http.MethodGet)
+	v1.HandleFunc("/book/search", bookHandler.SearchBook).Methods(http.MethodGet)
 }
